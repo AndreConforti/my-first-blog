05a546dd09c1e41dd4edb736cc1c7899a61dac6f
AndreConforti <andre.r.m.conforti@gmail.com>
2023-03-04 22:47:14 -0300

    Alterado o banco para slqite3. Mais fácil para configurar. E adicionada uma dica muito importante sobe TDD

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_bank',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'my_bank.slqite3',
     }
 }
 
